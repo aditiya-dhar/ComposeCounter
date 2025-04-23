b1319b4df448313d590b421b5f82dc543d5a9974
adi <tut25650@temple.edu>
2025-04-23 15:46:47 -0400

    added up and down arrow buttons,  functionality to add and subtract 1 with each click

--- app/src/main/java/edu/temple/counter/MainActivity.kt
+++ app/src/main/java/edu/temple/counter/MainActivity.kt
@@ -8,15 +8,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import edu.temple.counter.ui.theme.CounterTheme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.painterResource
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,12 +42,22 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Counter(modifier: Modifier = Modifier) {
+    var number by rememberSaveable {
+        mutableIntStateOf(0)
+    }
+
     Column (modifier = modifier.fillMaxSize(), verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally) {
+        Button({number++}) {
+            Icon(painterResource(android.R.drawable.arrow_up_float), contentDescription = "Up")
+        }
         Text(
-            text = "0",
+            text = number.toString(),
             fontSize = 192.sp
         )
+        Button({number--}) {
+            Icon(painterResource(android.R.drawable.arrow_down_float), contentDescription = "Down")
+        }
     }
 }
 
